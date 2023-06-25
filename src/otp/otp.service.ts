7d4347f9c497f54001837b2995c3c6b9043fd901
krishbale <sushil.pokharel.7528@gmail.com>
2023-06-25 19:18:22 +0545

    formatted spacing error

--- src/otp/otp.service.ts
+++ src/otp/otp.service.ts
@@ -6,16 +6,19 @@ import { OTP } from './enitity/otp.entity';
 
 @Injectable()
 export class OtpService {
-  constructor(private readonly dataSource: DataSource) { }
-
+  constructor(private readonly dataSource: DataSource) {}
 
   async createOTP(user_id: string, otp_type: OTPType) {
     const code = generateOTP(6);
-    return await this.dataSource.getRepository(OTP).save({ code, user_id, otp_type });
+    return await this.dataSource
+      .getRepository(OTP)
+      .save({ code, user_id, otp_type });
   }
 
   async deleteOTP(user_id: string, code: string, otp_type: OTPType) {
-    return await this.dataSource.getRepository(OTP).delete({ code, user_id, otp_type });
+    return await this.dataSource
+      .getRepository(OTP)
+      .delete({ code, user_id, otp_type });
   }
 
   async findLastOTP(user_id: string, otp_type: OTPType) {
@@ -24,15 +27,15 @@ export class OtpService {
       .findOne({ where: { user_id, otp_type }, order: { created_at: 'DESC' } });
   }
 
-  
-
-
   async validateOTP(user_id: string, code: string, otp_type: OTPType) {
-    const otp = await this.dataSource.getRepository(OTP).findOne({ where: { user_id, code, otp_type } });
+    const otp = await this.dataSource
+      .getRepository(OTP)
+      .findOne({ where: { user_id, code, otp_type } });
     if (!otp) throw new BadRequestException('Invalid OTP.');
 
     const expiryTime = 1000 * 60 * 15; // 15 minutes
-    if (otp.created_at.getTime() + expiryTime < Date.now()) throw new BadRequestException('OTP expired.');
+    if (otp.created_at.getTime() + expiryTime < Date.now())
+      throw new BadRequestException('OTP expired.');
     return true;
   }
 }
