a7b0590a2de2490347e7854f0078c9987b82b4e5
krishbale <sushil.pokharel.7528@gmail.com>
2023-07-05 11:37:48 +0545

    changing ecb

--- src/chat/entity/custom-transfer.ts
+++ src/chat/entity/custom-transfer.ts
@@ -1,27 +1,30 @@
 import { ValueTransformer } from 'typeorm';
 import * as crypto from 'crypto';
 
-const algorithm = 'aes-256-ctr';
-const Securitykey = 'e41c966f21f9e1577802463f8924e6a3';
+const algorithm = 'aes-256-ecb';
+const securityKey = 'e41c966f21f9e1577802463f8924e6a3';
 
 export class MyEncryptionTransformer implements ValueTransformer {
   to(value: string): string {
-    const initVector = crypto.randomBytes(16); // Initialization vector
-    const cipher = crypto.createCipheriv(algorithm, Securitykey, initVector);
-    let encryptedData = cipher.update(value, 'utf-8', 'hex');
-    encryptedData += cipher.final('hex');
-    return initVector.toString('hex') + encryptedData;
+    console.log(value);
+    const cipher = crypto.createCipheriv(
+      algorithm,
+      securityKey,
+      Buffer.alloc(0),
+    ); // Use an empty Buffer as IV for ECB mode
+    let encryptedData = cipher.update(value, 'utf-8', 'base64');
+    encryptedData += cipher.final('base64');
+    return encryptedData;
   }
 
   from(value: string): string {
-    const initVector = Buffer.from(value.substring(0, 32), 'hex'); // Extract the initialization vector
-    const cipherText = value.substring(32);
+    console.log(value);
     const decipher = crypto.createDecipheriv(
       algorithm,
-      Securitykey,
-      initVector,
-    );
-    let decryptedData = decipher.update(cipherText, 'hex', 'utf-8');
+      securityKey,
+      Buffer.alloc(0),
+    ); // Use an empty Buffer as IV for ECB mode
+    let decryptedData = decipher.update(value, 'base64', 'utf-8');
     decryptedData += decipher.final('utf-8');
     return decryptedData;
   }
