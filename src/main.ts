624c3bdbde4286a5e832bbfd86ca59e6151d3128
krishbale <sushil.pokharel.7528@gmail.com>
2024-01-20 22:34:58 +0545

    updated

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 // import { ValidationPipe } from '@nestjs/common';
 import * as compression from 'compression';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(compression());
@@ -11,6 +12,16 @@ async function bootstrap() {
   //     whitelist: true,
   //   }),
   // );
-   app.listen(3000);
+
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: '*',
+    credentials: true,
+    optionsSuccessStatus: 204,
+  };
+
+  app.enableCors(corsOptions);
+
+  await app.listen(3000);
 }
 bootstrap();
