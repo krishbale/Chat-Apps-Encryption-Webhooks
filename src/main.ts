52111c3da6a545cbd19e88cd50ff6508c7649ac6
krishbale <sushil.pokharel.7528@gmail.com>
2024-01-20 20:19:56 +0545

    Revert "updated swagger ui"
    This reverts commit 9a3df6a35f6781f7658c074e74ab5dd0b72acbfc.

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as compression from 'compression';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(compression());
-  const config = new DocumentBuilder()
-  .setTitle('Chat Application')
-  .setDescription('The Api for chat applicaton')
-  .setVersion('1.0')
-  .addTag('chat')
-  .build();
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
